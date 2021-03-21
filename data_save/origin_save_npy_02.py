36ee7f5d7163466542694e9700939955a11a1825
YoungriKIM <lemontleo0311@gmail.com>
2021-03-22 02:17:51 +0900

    2021-03-22

---
+++ data_save/origin_save_npy_02.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+import PIL.Image as pilimg
+
+# 1000개 라벨과 7200개의 pred로 저장해서 진행!
+
+# ---------------------------------------------------------------------
+# train 데이터 불러오기
+train = list()
+label = list()
+number1 = 1000
+number2 = 48
+
+# 이미지 저장하면서 라벨까지 저장
+# train
+# train - label 
+for aa in range(number1):
+    for a in range(number2):
+        temp = cv2.imread('D:/lotte_data/LPD_competition/train/' + str(aa)+ '/' + str(a) + '.jpg')
+        temp = cv2.resize(temp, (128, 128))
+        temp = np.array(temp)
+        train.append(temp)
+        label.append(aa)
+
+# np.array로 바꿔서 쉐잎 확인
+train_data_1000 = np.array(train)
+train_label_1000 = np.array(label)
+print(train_data_1000.shape)
+print(train_label_1000.shape)
+
+# ---------------------------------------------------------------------
+# pred 저장
+pred = list()
+number3 = 72000
+for b in range(number3):
+    temp = cv2.imread('D:/lotte_data/LPD_competition/test/' + str(b) + '.jpg')
+    temp = cv2.resize(temp, (128, 128))
+    temp = np.array(temp)
+    pred.append(temp)
+
+# np.array로 바꿔서 쉐잎 확인
+pred_data_72000 = np.array(pred)
+print(pred_data_72000.shape)
+
+
+# ---------------------------------------------------------------------
+# npy로 저장
+
+np.save('C:/lotte_data/LPD_competition/npy/train_data_1000.npy', arr = train_data_1000)
+np.save('C:/lotte_data/LPD_competition/npy/train_label_1000.npy', arr = train_label_1000)
+np.save('C:/lotte_data/LPD_competition/npy/pred_data_72000.npy', arr = pred_data_72000)
+print('===== done =====')
--- memo/memo_01.py
+++ memo/memo_01.py
@@ -1,37 +1,6 @@
-from sklearn.linear_model import LinearRegression  
-from sklearn.preprocessing import PolynomialFeatures 
-from sklearn.metrics import mean_squared_error, r2_score
+class_correct = 0
 
-import matplotlib.pyplot as plt
-import numpy as np
-import random
 
-#----------------------------------------------------------------------------------------#
-# Step 1: training data
-
-X = [i for i in range(10)]
-Y = [random.gauss(x,0.75) for x in X]
-
-X = np.asarray(X)
-Y = np.asarray(Y)
-
-X = X[:,np.newaxis]
-Y = Y[:,np.newaxis]
-
-plt.scatter(X,Y)
-
-#----------------------------------------------------------------------------------------#
-# Step 2: data preparation
-
-nb_degree = 4
-
-polynomial_features = PolynomialFeatures(degree = nb_degree)
-
-X_TRANSF = polynomial_features.fit_transform(X)
-
-#----------------------------------------------------------------------------------------#
-# Step 3: define and train a model
-
-model = LinearRegression()
-
-model.fit(X_TRANSF, Y)
+for i in range(4):
+    class_correct += 1
+print(class_correct)
