84d994968402a08dec2b9935dbeba88f17f58157
YoungriKIM <lemontleo0311@gmail.com>
2021-03-28 23:14:26 +0900

    2021-03-28

---
+++ winner's code/1st_code.py
@@ -0,0 +1,2 @@
+# 1등 깃헙이랑 코드
+# https://github.com/DShomin/Lotte_product_classification
---
+++ winner's code/2nd_code.py
@@ -0,0 +1,5 @@
+# 롯데정보통신 Vision AI 경진대회 - Public LB 2nd place Solution
+# https://dev-hunmin.tistory.com/entry/%EB%A1%AF%EB%8D%B0%EC%A0%95%EB%B3%B4%ED%86%B5%EC%8B%A0-Vision-AI-%EA%B2%BD%EC%A7%84%EB%8C%80%ED%9A%8C-Public-LB-2nd-place-Solution
+
+# 깃허브
+# https://github.com/hunmin-hub/LotteVisionAI
